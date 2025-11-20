7b7c68dff9c72eb8234ae5dc0088117882b746bb
VinothiniGit <vinothini4122001@gmail.com>
2025-11-20 15:58:08 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,34 @@ app.get('/',(req,res) => {
     res.json({msg : "Hello world"});
 });
 
+
+//CRUD functionality of movies
+//R-For Reading
+
+app.get('/movies',() => {
+
+});
+
+
+//C - For Creating Movies
+
+app.post('/movies',() =>{
+
+});
+//U-For updating movies 
+
+app.put('/movies/:id',() =>{
+
+});
+
+//D-delete the movies
+
+app.delete('/movies/:id',() => {
+
+});
+
+
+
 app.listen(PORT,() => {
     console.log(`The server is running at http://localhost:${PORT}`);
 });
